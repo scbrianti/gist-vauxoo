f65ae13660ca94fd318309fa2e28bbadda361def
Humberto Arocha <hbto@vauxoo.com>
2016-11-10 19:36:29 -0400

    [IMP] Updating Products with Cost Method = Standard

--- lodi-ws-updateSTD.py
+++ lodi-ws-updateSTD.py
@@ -6,44 +6,6 @@ import click
 import psycopg2
 import dagger
 
-QUERY_MATERIAL = '''
-    SELECT
-        sm.id AS sm_id
-    FROM stock_move AS sm
-    INNER JOIN
-        stock_location AS sl_src ON sm.location_id = sl_src.id
-    INNER JOIN
-        stock_location AS sl_dst ON sm.location_dest_id = sl_dst.id
-    WHERE
-        sm.state = 'done' -- Stock Move already DONE
-        AND sl_src.usage = '{where}'
-        AND sl_dst.usage = 'internal'
-'''
-
-QUERY = '''
-    SELECT
-        DISTINCT(sq.id) AS quant_id
-    FROM stock_quant AS sq
-    INNER JOIN
-        stock_quant_move_rel AS sqm_rel ON sqm_rel.quant_id = sq.id
-    INNER JOIN
-        stock_move AS sm ON sqm_rel.move_id = sm.id
-    INNER JOIN
-        stock_location AS sl_src ON sm.location_id = sl_src.id
-    INNER JOIN
-        stock_location AS sl_dst ON sm.location_dest_id = sl_dst.id
-    WHERE
-        sm.state = 'done' -- Stock Move already DONE
-        AND sl_src.usage != sl_dst.usage -- No self transfers
-        AND (
-            (sl_src.usage = 'internal' AND sl_dst.usage != 'internal')
-            OR (
-            sl_src.usage != 'internal' AND sl_dst.usage = 'internal')
-        )
-        AND sl_src.usage = 'production'
-        AND sm.production_id IS NOT NULL;
-'''
-
 _logger = logging.getLogger(__name__)
 #############################
 # ## Constants Declaration ###
@@ -81,72 +43,53 @@ def run(
     return True
 
 
-def get_dag_product(connect, conn_pg):
+def get_products(connect, conn_pg):
+    """Return all products which represent top parent in bom
+    [x]---+   [y]
+     |    |  / |
+     |    | /  |
+    [a]  [b]  [c]
+     |  / |
+     | /  |
+    [t]  [u]
+    That is x and y
+    """
     res = {}
     dag = None
     cr = conn_pg.cursor()
 
     cr.execute('''
         SELECT
-            DISTINCT pt.id AS pt1,
-            pt2.id AS pt2
-        FROM mrp_production mp
-        INNER JOIN product_product AS pp ON pp.id = mp.product_id
-        INNER JOIN product_template AS pt ON pt.id = pp.product_tmpl_id
-        INNER JOIN mrp_bom AS mb ON mb.product_tmpl_id = pt.id
-        INNER JOIN mrp_bom_line AS mbl ON mbl.bom_id = mb.id
-        INNER JOIN product_product AS pp2 ON pp2.id = mbl.product_id
-        INNER JOIN product_template AS pt2 ON pt2.id = pp2.product_tmpl_id
-        WHERE mp.state = 'done';
+        DISTINCT mb.product_tmpl_id AS pp1,
+                pp.product_tmpl_id AS pp2
+        FROM mrp_bom AS mb
+        INNER JOIN mrp_bom_line as mbl ON mbl.bom_id = mb.id
+        INNER JOIN product_product as pp ON pp.id = mbl.product_id
+        WHERE mb.active = True
+        ORDER BY pp1;
             ''')
     result = cr.fetchall()
+    parents = set([r[0] for r in result if r[0] is not None])
+    children = set([r[1] for r in result if r[1] is not None])
+    root = list(parents - children)
 
-    if not result:
-        return res, dag
-
-    for pt1, pt2 in result:
-        pt1, pt2 = str(pt1), str(pt2)
-        if not res.get(pt1):
-            res[pt1] = set([pt2])
-            continue
-        res[pt1].add(pt2)
-
-    dag = dagger.dagger()
-    for key, val in res.iteritems():
-        dag.add(key, list(val))
-    dag.run()
-    res = dag.ordernames()
-    res = res.split(',')
-    res = [int(val) for val in res]
-
-    return res, dag
+    cr.execute('''
+        SELECT DISTINCT id
+        FROM product_product
+        WHERE product_tmpl_id IN %s''', (tuple(root),))
+    root = cr.fetchall()
+    return list(set(res[0] for res in root))
 
 
 def update_std_price(connect, conn_pg):
-    product_ids, dag = get_dag_product(connect, conn_pg)
-    print len(product_ids)
-    context = {}
-    values = {
-        'real_time_accounting': True,
-        'recursive': True}
-    count = 0
-    for product in product_ids:
-        count += 1
-        path = dag.pathnames(str(product))
-        print '{product} {count} / {total}'.format(
-            product=product, count=count, total=len(product_ids))
-        if path:
-            print product, 'NODE'
-            continue
-        else:
-            print product, 'ROOT'
-        context.update({'active_model': 'product.template',
-                        'active_id': product})
-        wzd_price_id = connect.execute(
-            'wizard.price', 'create', values, context)
+    product_ids = get_products(connect, conn_pg)
+    if not product_ids:
+        print 'No Root Products to Update'
+        return True
 
+    for product in product_ids:
         connect.execute(
-            'wizard.price', 'compute_from_bom', [wzd_price_id], context)
+            'wizard.price', 'execute_cron', [product])
     return True
 
 
